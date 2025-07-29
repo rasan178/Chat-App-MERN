e56cf6f8caa6bf8e2669ad62a19c6d1bcedbeb28
Rasan Samarakkody <rasansamarakkody077@gmail.com>
2025-07-29 22:32:09 +0530

    cors fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-mern-rasan-samarakkodys-projects.vercel.app",
     credentials: true,
   })
 );
