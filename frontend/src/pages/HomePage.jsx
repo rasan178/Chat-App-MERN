83c30972a913a2ad84406b626dede4762d2a8ce1
Rasan Samarakkody <rasansamarakkody077@gmail.com>
2025-07-30 12:41:14 +0530

    overlap issue

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
 
   return (
     <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-4 sm:pt-8 md:pt-12 lg:pt-20 px-2 sm:px-4">
+      <div className="flex items-center justify-center pt-4 sm:pt-8 md:pt-12 lg:pt-20 px-2 sm:px-4 lg:pl-22 xl:pl-66">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-2rem)] sm:h-[calc(100vh-4rem)] md:h-[calc(100vh-6rem)] lg:h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
